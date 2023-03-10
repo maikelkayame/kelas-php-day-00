d7dbd1b2ee78cbf1816e7b2cdeadf67419437f68
unknown <kayamebate@gmail.com>
2023-03-10 10:49:10 +0900

    coding php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	//variabel String
+	$judul="hello word!";
+	$bilangan = 10;
+	$nama ="Hello Maikel";
+	$nm="jefri";
+	
+?>
+
+<html>
+
+<head>
+<title> php day 00 </title>
+
+</head>
+<body>
+<h1>php day 00-file php</h1>
+<h2> <?php echo $judul; ?> </h2>
+<h2> <?= $bilangan ?> </h2>
+<h2> <?= $nama ?></h2>
+<h2> <?= "hello".$nm ?> </h2>
+</body>
+</html>
